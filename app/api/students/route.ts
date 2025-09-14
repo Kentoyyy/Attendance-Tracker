7b909930ba901422f9e97167f3fbc34e71883873
_Kentoyyy <69066165+Kentoyyy@users.noreply.github.com>
2025-09-14 17:45:20 +0800

    up

--- app/api/students/route.ts
+++ app/api/students/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/lib/auth';
-import prisma from '@/app/lib/prisma';
+import { prisma } from '@/app/lib/prisma';
 
 export const dynamic = 'force-dynamic';
 
